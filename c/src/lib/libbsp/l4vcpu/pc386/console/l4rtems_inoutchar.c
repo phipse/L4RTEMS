023ec2229455ef5970cb6946ae667b9bb1e8708f
Philipp Eppelt <philipp88@eppelt.de>
2013-01-28 13:38:09 +0100

    buffered output

--- c/src/lib/libbsp/l4vcpu/pc386/console/l4rtems_inoutchar.c
+++ c/src/lib/libbsp/l4vcpu/pc386/console/l4rtems_inoutchar.c
@@ -1,5 +1,5 @@
 
-
+#include <string.h>
 #include <rtems/l4vcpu/l4vcon.h>
 /* L4RTEMS: include the wrapper.h to get the sharedVariableStruct */
 #include <rtems/score/wrapper.h>
@@ -9,8 +9,23 @@ extern sharedvars_t *sharedVariableStruct;
 
 l4_fastcall void
 l4rtems_outch( const char c )
-{ // write one byte long 'c' to the log
-  l4_vcon_write( sharedVariableStruct->logcap, &c, 1 );
+{ // buffer output until newline or null and then print to log
+  static char buf_out[ 512 ];
+  static int i = 0;
+
+  if( c == '\n'  || c == 0 )
+  {
+    buf_out[i] = '\n';
+    l4_vcon_write( sharedVariableStruct->logcap, buf_out, ++i );
+    memset( buf_out, 0, i );
+    i = 0;
+  }
+  else
+  {
+    buf_out[i] = c;
+    ++i;
+  }
+  //l4_vcon_write( sharedVariableStruct->logcap, &c, 1 );
 }
 
 l4_fastcall int
